aa3711aa4c00a9b5f6ed4df27eea2017b8dbd29e
xinglie <xinglie.lkf@taobao.com>
2012-12-15 20:51:05 +0800

    test

--- scripts/tool/_.cmd
+++
@@ -1,8 +0,0 @@
-@echo off
-set cp=%cd%
-
-cd ../../
-
-for /r %cd% %%a in (*.source.js) do (
-     %cp%\compiler\compiler.cmd %%a
-)
--- scripts/tool/_rls.cmd
+++
@@ -1,3 +0,0 @@
-@echo off
-set cp=%cd%
-cmd /c "_.cmd & cd %cp% & cscript _.js"
